c10513219a40c2b1f05b1d8e7ba6f9fe41f9bb71
Zajs <aga.zajs@gmail.com>
2017-08-17 18:58:31 +0300

    Refactored build script

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
+
+VERSION=$1
+
+[ -z "$1" ] && VERSION="0.0.1"
+
 echo "Building dependencies and Docker images for demo..."
 
 echo "Runtimes:"
@@ -6,23 +12,23 @@ cd runtimes
 echo "Spark Local ML..."
 cd localml-spark
 sbt assembly
-docker build --build-arg VERSION=0.0.1 --no-cache -t hydro-serving/runtime-sparklocal .
+docker build --build-arg VERSION=$VERSION --no-cache -t hydro-serving/runtime-sparklocal .
 cd ../
 
 echo "Scikit..."
 cd scikit
-docker build --build-arg VERSION=0.0.1 -t mist-envoy-alpine-python-machinelearning -f Dockerfile-alpine-python-machinelearning .
-docker build --build-arg VERSION=0.0.1 --no-cache -t hydro-serving/runtime-scikit .
+docker build --build-arg VERSION=$VERSION -t mist-envoy-alpine-python-machinelearning -f Dockerfile-alpine-python-machinelearning .
+docker build --build-arg VERSION=$VERSION --no-cache -t hydro-serving/runtime-scikit .
 cd ../
 
 echo "Custom Scikit..."
 cd custom_scikit
-docker build --build-arg VERSION=0.0.1 --no-cache -t hydro-serving/runtime-customscikit .
+docker build --build-arg VERSION=$VERSION --no-cache -t hydro-serving/runtime-customscikit .
 cd ../
 
 echo "Tensorflow..."
 cd tensorflow
-docker build --build-arg VERSION=0.0.1 --no-cache -t hydro-serving/runtime-tensorflow -f Dockerfile-tensorflow-cpu .
+docker build --build-arg VERSION=$VERSION --no-cache -t hydro-serving/runtime-tensorflow -f Dockerfile-tensorflow-cpu .
 cd ../
 
 cd ../
