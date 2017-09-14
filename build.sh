75fd12f7707ee17e7234eb608390e2a330b3ae87
Zajs <aga.zajs@gmail.com>
2017-09-14 22:14:19 +0300

    Added separate argument - SIDECAR_VERSION

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@
 
 VERSION=$1
 
+SIDECAR_VERSION=$2
+
 [ -z "$1" ] && VERSION="0.0.1"
+[ -z "$2" ] && SIDECAR_VERSION="0.0.1"
 
 echo "Building dependencies and Docker images for demo..."
 
@@ -12,23 +15,23 @@ cd runtimes
 echo "Spark Local ML..."
 cd localml-spark
 ./sbt/sbt assembly
-docker build --build-arg VERSION=$VERSION --no-cache -t hydro-serving/runtime-sparklocal:$VERSION .
+docker build --build-arg VERSION=$VERSION --build-arg SIDECAR_VERSION=$SIDECAR_VERSION --no-cache -t hydro-serving/runtime-sparklocal:$VERSION .
 cd ../
 
 echo "Scikit..."
 cd scikit
-docker build --build-arg VERSION=$VERSION -t mist-envoy-alpine-python-machinelearning:$VERSION -f Dockerfile-alpine-python-machinelearning .
-docker build --build-arg VERSION=$VERSION --no-cache -t hydro-serving/runtime-scikit:$VERSION .
+docker build --build-arg VERSION=$VERSION --build-arg SIDECAR_VERSION=$SIDECAR_VERSION -t mist-envoy-alpine-python-machinelearning:$VERSION -f Dockerfile-alpine-python-machinelearning .
+docker build --build-arg VERSION=$VERSION --build-arg SIDECAR_VERSION=$SIDECAR_VERSION --no-cache -t hydro-serving/runtime-scikit:$VERSION .
 cd ../
 
 echo "Custom Scikit..."
 cd custom_scikit
-docker build --build-arg VERSION=$VERSION --no-cache -t hydro-serving/runtime-customscikit:$VERSION .
+docker build --build-arg VERSION=$VERSION --build-arg SIDECAR_VERSION=$SIDECAR_VERSION --no-cache -t hydro-serving/runtime-customscikit:$VERSION .
 cd ../
 
 echo "Tensorflow..."
 cd tensorflow
-docker build --build-arg VERSION=$VERSION --no-cache -t hydro-serving/runtime-tensorflow:$VERSION -f Dockerfile-tensorflow-cpu .
+docker build --build-arg VERSION=$VERSION --build-arg SIDECAR_VERSION=$SIDECAR_VERSION --no-cache -t hydro-serving/runtime-tensorflow:$VERSION -f Dockerfile-tensorflow-cpu .
 cd ../
 
 cd ../
