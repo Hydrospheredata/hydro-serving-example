ce56c9419a0a742e0521b46e748560836eca5fea
Rustem Zakiev <rustemzakiev@MacBook-Pro.local>
2019-03-16 15:37:51 +0300

    created app to trigger scoring

---
+++ demos/LightGBM/app.py
@@ -0,0 +1,36 @@
+import grpc;
+import hydro_serving_grpc as hs;
+import json;
+
+cfg_file = open("config.json", "r");
+cfg = json.loads(cfg_file.read());
+cfg_file.close();
+
+def getData(filename, size = 5):
+	data_file = open(filename, "r+");
+	data_list = data_file.readlines();
+
+	return data_list[0:size];
+
+def main():
+	channel = grpc.insecure_channel(cfg["url"]); 
+	stub = hs.PredictionServiceStub(channel); 
+
+	data = getData("samples.json");
+	dim_size = len(data);
+	print(data);
+
+	model_spec = hs.ModelSpec(name="favorita", signature_name="favorita");
+
+	tensor_shape = hs.TensorShapeProto(dim=[hs.TensorShapeProto.Dim(size=dim_size)]);
+
+	tensor = hs.TensorProto(dtype=hs.DT_STRING, tensor_shape=tensor_shape, string_val=data)
+
+	request = hs.PredictRequest(model_spec=model_spec, inputs={"input_data": tensor}) 
+
+	result = stub.Predict(request)
+
+	return result;
+
+if __name__ == "__main__":
+	print(repr(main()));
