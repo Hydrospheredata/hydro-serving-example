b8fc63c378a23615c39d1ef61901c43d98d81c5e
Beardedwhale <Oldman2016>
2019-08-16 16:12:50 +0300

    Add face recognition models

---
+++ examples/fraud_detection/model/src/func_main.py
@@ -0,0 +1,19 @@
+import hydro_serving_grpc as hs
+import numpy as np
+from sklearn.externals import joblib
+
+clf = joblib.load('/model/files/rf.joblib.pkl')
+
+
+def infer(features):
+    data = np.array(features.double_val) \
+        .reshape([dim.size for dim in features.tensor_shape.dim])
+    prediction = clf.predict(data)
+
+    guess_shape = hs.TensorShapeProto(
+        dim=[hs.TensorShapeProto.Dim(size=item) for item in prediction.shape])
+    guess = hs.TensorProto(
+        dtype=hs.DT_BOOL,
+        bool_val=prediction,
+        tensor_shape=guess_shape)
+    return hs.PredictResponse(outputs={'is_fraud': guess})
