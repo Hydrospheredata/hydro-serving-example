10485fcc07e12873b654ad6f760fdd47faadb93e
Nikita <33264275+GurovNik@users.noreply.github.com>
2019-08-22 20:59:25 +0300

    Add files via upload

---
+++ examples/adult/grpc_request_ad.py
@@ -0,0 +1,34 @@
+import grpc 
+import numpy as np
+import hydro_serving_grpc as hs
+from grpc import ssl_channel_credentials
+
+#            import hydro_serving_grpc as hs 
+
+channel = grpc.secure_channel("dev.k8s.hydrosphere.io", credentials=ssl_channel_credentials())
+stub = hs.PredictionServiceStub(channel) 
+model_spec = hs.ModelSpec(name="adult_test", signature_name="INFERRED_PIPELINE_SIGNATURE")
+tensor_shape = hs.TensorShapeProto(dim=[hs.TensorShapeProto.Dim(size=-1), hs.TensorShapeProto.Dim(size=12)])
+val = np.array([
+    [
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1
+    ]
+  ], dtype="int64").flatten()
+print(val.shape)
+# val = val.flatten()
+
+tensor = hs.TensorProto(dtype=hs.DT_INT64, tensor_shape=tensor_shape, int64_val=val)
+request = hs.PredictRequest(model_spec=model_spec, inputs={"input": tensor}) 
+result = stub.Predict(request)
+print(result)
