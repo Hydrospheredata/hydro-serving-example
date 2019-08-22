18351374521614491e9422699de65bccd81e613d
Nikita <33264275+GurovNik@users.noreply.github.com>
2019-08-22 23:33:28 +0300

    Update func_main.py

--- examples/python_mnist/model/src/func_main.py
+++ examples/python_mnist/model/src/func_main.py
@@ -3,20 +3,16 @@ import numpy as np
 import tensorflow as tf
 from keras.models import load_model
 
-
 def extract_value(proto):
     return np.array(proto.double_val, dtype='float64').reshape((-1, 28 * 28))
-
 
 m = load_model("/model/files/model.h5", compile=False)
 
 global graph
 graph = tf.get_default_graph()
 
-
 def predict(**kwargs):
     extracted = extract_value(kwargs['input'])
-
     with graph.as_default():
         probas = m.predict(extracted)
 
