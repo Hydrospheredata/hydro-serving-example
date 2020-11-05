50f910243708c45b9c528f013ad9a26ea60a3d17
Ilnur Garifullin <ilnur.email@gmail.com>
2020-11-05 12:20:20 +0300

    Prepare activity recognition example

--- examples/activity_recognition/model/src/func_main.py
+++ examples/activity_recognition/model/src/func_main.py
@@ -1,31 +1,12 @@
-import keras
 import numpy as np
 import tensorflow as tf
-import hydro_serving_grpc as hs
 from keras.models import load_model
 
-# 0. Load model once
-model = load_model('/model/files/model.h5')
 
-global graph
-graph = tf.get_default_graph() #this is a workaround of keras' issue with multithreading
+model = load_model('/model/files/model.h5')
+graph = tf.get_default_graph() # this is a workaround of keras' issue with multithreading
+
 
 def infer(x):
-    # 1. Retrieve tensor's content and put it to numpy array
-    data = np.array(x.double_val)
-    data = data.reshape([dim.size for dim in x.tensor_shape.dim])
-
-    # 2. Make a prediction
     with graph.as_default():
-        result = model.predict(data)
-    print (result)
-    # 3. Pack the answer
-    y_shape = hs.TensorShapeProto(dim=[hs.TensorShapeProto.Dim(size=5)])
-    y_tensor = hs.TensorProto(
-        dtype=hs.DT_DOUBLE,
-        double_val=result.flatten(),
-        tensor_shape=y_shape)
-
-    # 4. Return the result
-    return hs.PredictResponse(outputs={"y": y_tensor})
-
+        return {"y": model.predict(x)}
